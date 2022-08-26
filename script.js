07ecd5d91c76f9bb97e23f98d9814b0f9a751e66
OfficialDustman <84110235+OfficialDustman@users.noreply.github.com>
2022-08-26 10:01:22 +0100

    updated and made ui responsive, add modal images

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
-header = document.querySelector('nav')
+header = document.querySelector('header')
+modal = document.getElementById("myModal")
+img = document.querySelectorAll('.img1 img')
+img2 = document.querySelectorAll('.img2 img')
+console.log(img);
+modalImg = document.getElementById("img01")
+caption = document.getElementById("caption")
 
-console.log(header.style);
+scrollTrigger = 60;
 
 window.onscroll = function() {
-    header.style.backgroundColor = 'white';
-    header.style.boxShadow = '0 2px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%)'
-    console.log(header.style.boxShadow);
+
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+      header.style.backgroundColor = 'white'
+      header.style.boxShadow = '0 2px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%)'
+  } else {
+      header.style.backgroundColor = 'transparent'
+      header.style.boxShadow = 'none'
+  }
+}
+
+img.forEach((imgs) => {
+
+  imgs.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    caption.innerHTML = this.alt;
+  }
+});
+
+img2.forEach((imgs) => {
+
+  imgs.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    caption.innerHTML = this.alt;
+  }
+});
+
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
